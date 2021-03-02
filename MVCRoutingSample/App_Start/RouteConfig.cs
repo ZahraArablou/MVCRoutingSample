089b15ccb584cb870b5005f644c630f760456c98
Ricardo Artola <rartola@outlook.com>
2021-03-02 11:10:52 -0500

    Add project files.

---
+++ MVCRoutingSample/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVCRoutingSample
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Blog",
+                url: "{year}/{month}/{day}/{title}",
+                defaults: new { controller = "Blog", action = "ByDate" },
+                constraints:
+                new { year = @"\d{4}", month = @"\d{2}", day = @"\d{2}" }
+                );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MVCRoutingSample/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using MVCRoutingSample.Models;
+using System.Web.Mvc;
+
+namespace MVCRoutingSample.Controllers
+{
+    public class BlogController : Controller
+    {
+        [HttpGet]
+        public ActionResult ByDate(Blog blog)
+        {
+            /*ViewBag.Message =
+                string.Format("The blog {3} for the year:{0}, month:{1}, and day:{2} has been visited",
+                year, month, day, title);*/
+
+            return View(blog);
+        }
+    }
+}
